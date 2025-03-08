726f30d1e7f9863941e66e065cc6ce493ceebd5b
Mbugua <mwaurathealex@gmail.com>
2025-03-08 12:41:34 +0300

    meta and img alt

--- src/components/csvg.tsx
+++ src/components/csvg.tsx
@@ -2,5 +2,5 @@
 import csvg from "../assets/C.svg"
 
 export function cSVG() {
-  return (<img class="size-8" src={csvg} />)
+  return (<img class="size-8" alt="c programming language" src={csvg} />)
 }
